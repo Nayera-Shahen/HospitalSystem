bcb7402b8452a4ab9948e5936c5ec2d5edc526d0
Nayera-Shahen <122104652+Nayera-Shahen@users.noreply.github.com>
2024-11-23 16:49:34 +0200

    Add files via upload

---
+++ backend/Data/HospitalDbContext.cs
@@ -0,0 +1,96 @@
+﻿using HospitalSystemAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalSystemAPI.Data
+{
+    public class HospitalDbContext: IdentityDbContext<ApplicationUser>
+    {
+        public HospitalDbContext(DbContextOptions<HospitalDbContext> options) :
+            base(options)
+        {
+
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<DoctorSchedule> DoctorsSchedules { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<MedicalHistory> MedicalHistories { get; set; }
+        public DbSet<MedicalRecord> MedicalRecords { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Seed Specialties
+            modelBuilder.Entity<Speciality>().HasData(
+                new Speciality { Id = 1, Name = "Pediatrics" },
+                new Speciality { Id = 2, Name = "Cardiology" },
+                new Speciality { Id = 3, Name = "Orthopedics" },
+                new Speciality { Id = 4, Name = "Neurology" },
+                new Speciality { Id = 5, Name = "Dermatology" },
+                new Speciality { Id = 6, Name = "Neurology" },
+                new Speciality { Id = 7, Name = "Endocrinology" },
+                new Speciality { Id = 8, Name = "Infectious Disease"},
+                new Speciality { Id = 9, Name = "Hematology" },
+                new Speciality { Id = 10, Name = "Anesthesiology" }
+            );
+
+            // Ensure the UserName is unique
+            modelBuilder.Entity<ApplicationUser>()
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+
+            // Ensure the Email is unique
+            modelBuilder.Entity<ApplicationUser>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            // Configure the Doctor and Speciality relationship
+            modelBuilder.Entity<Doctor>()
+                .HasOne(d => d.Speciality)
+                .WithMany(s => s.Doctors)
+                .HasForeignKey(d => d.SpecialityId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            //appointment and patient relation
+            modelBuilder.Entity<Appointment>()
+        .HasOne(a => a.Patient)
+        .WithMany(p => p.Appointments)
+        .HasForeignKey(a => a.PatientId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+            //appointment and doctor relation
+            modelBuilder.Entity<Appointment>()
+                .HasOne(a => a.Doctor)
+                .WithMany(d => d.Appointments)
+                .HasForeignKey(a => a.DoctorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            //medical record and patient relation
+            modelBuilder.Entity<MedicalRecord>()
+        .HasOne(a => a.Patient)
+        .WithMany(p => p.MedicalRecords)
+        .HasForeignKey(a => a.PatientId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+            //medical record and doctor relation
+            modelBuilder.Entity<MedicalRecord>()
+                .HasOne(a => a.Doctor)
+                .WithMany(d => d.MedicalRecords)
+                .HasForeignKey(a => a.DoctorId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Patient>()
+            .Property(p => p.Id)
+            .ValueGeneratedNever();
+
+            modelBuilder.Entity<Doctor>()
+            .Property(d => d.Id)
+            .ValueGeneratedNever();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
